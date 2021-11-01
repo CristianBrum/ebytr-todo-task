1e5688496418028d9c9eb6d4bca238486b48a2fc
cristian <cristian.brum2@yahoo.com.br>
2021-11-01 17:36:25 -0300

    feat: :rocket: adiciona validacoes nas rotas

--- server/validations/taskValidation.js
+++ server/validations/taskValidation.js
@@ -1,3 +1,4 @@
+const { ObjectId } = require('mongodb');
 const taskModel = require('../models/taskmodel');
 
 const checkSmallTask = (req, res, next) => {
@@ -44,8 +45,22 @@ const getTaskByName = async (req, res, next) => {
   return next();
 };
 
+const checkTaskId = (req, res, next) => {
+  const { id } = req.params;
+  if (!ObjectId.isValid(id)) {
+    return res.status(422).json({
+      err: {
+        code: 'invalid_data',
+        message: 'Wrong id format',
+      },
+    });
+  }
+  return next();
+};
+
 module.exports = {
   checkSmallTask,
   checkBigTask,
   getTaskByName,
+  checkTaskId,
 };
