f5b27efb6c006bff4807ec5cc9586127277d0851
cristian <cristian.brum2@yahoo.com.br>
2021-11-01 18:05:42 -0300

    feat: :rocket: cria validacoes de novo usuario

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const rescue = require('express-rescue');
 
+const isValid = require('../validations/userValidation');
+
 const userRoutes = express.Router();
 const userController = require('../controllers/userController');
 
 userRoutes.post('/',
+  isValid.isValidName,
+  isValid.isValidPassword,
+  isValid.isValidEmail,
+  isValid.userExists,
   rescue(userController.createNewUser));
 
 module.exports = userRoutes;
