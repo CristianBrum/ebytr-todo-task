718b14305bfc6905dc5e4cc007202737acc745c9
Cristian Brum <cristian.brum2@yahoo.com.br>
2021-11-05 04:28:44 -0300

    Delete routes directory

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const user = require('./userRoutes');
-const login = require('./loginRoutes');
-const task = require('./taskRoutes');
-
-module.exports = {
-  user,
-  login,
-  task,
-};
--- routes/loginRoutes.js
+++
@@ -1,9 +0,0 @@
-const loginRoutes = require('express').Router();
-const rescue = require('express-rescue');
-
-const loginController = require('../controllers/loginController');
-
-loginRoutes.post('/',
-  rescue(loginController.login));
-
-module.exports = loginRoutes;
